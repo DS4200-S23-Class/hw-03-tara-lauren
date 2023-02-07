ddb3f5a1addf202e2b3e6575273590973b5576e6
laurenmontion <119334132+laurenmontion@users.noreply.github.com>
2023-02-06 21:21:32 -0500

    added attributes, comments in js file

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// function to change background color once button is clicked
+// button initialized in html file
+
 function changeColor() {
   var element = document.body;
   element.classList.toggle("changed_col");
